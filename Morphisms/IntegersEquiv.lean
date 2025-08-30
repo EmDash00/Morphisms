ce0d645e55cce7682cf083231a159bd814dca6fe
Wispy Daydream <github.com.ounce967@passmail.net>
2025-08-29 17:45:21 -0700

    palindromes

--- Morphisms/IntegersEquiv.lean
+++ Morphisms/IntegersEquiv.lean
@@ -3,63 +3,33 @@ import Mathlib
 
 open Lean Elab Tactic
 
-def digitsWithFuel (base num fuel : Nat) : List Nat :=
-  match num, fuel with
-  | 0, _ => []
-  | _, 0 => []
-  | m + 1, n => n % base :: digitsWithFuel base m (n / base)
+def digit (base n : Nat) (k : Nat) : ℕ := (n / base^k) % base
 
+def binDigit (n k : Nat) : ℕ := digit 2 n k
 
-def digits (base n : Nat) : List Nat := (digitsWithFuel base n n).reverse
+def lsb (n : Nat) : ℕ := digit 2 n 0
 
-def hammingWeight (n : Nat) : Nat := (digits 2 n).sum
-
-def fromDigits (base : Nat) (digits : List Nat) : Nat :=
-  digits.foldl (λ acc digit => acc * base + digit) 0
-
-
-def concat (x y : ℕ) : Nat :=
-  fromDigits 10 ((digits 10 x) ++ (digits 10 y))
-
-def signedConcat : (x y : ℤ) → ℤ
-| Int.ofNat a, Int.ofNat b => Int.ofNat (fromDigits 10 ((digits 10 a) ++ (digits 10 b)))
-| Int.negSucc a, Int.negSucc b => Int.ofNat (fromDigits 10 ((digits 10 (Nat.succ a)) ++ (digits 10 (Nat.succ b))))
-| Int.negSucc a, Int.ofNat b => -Int.ofNat (fromDigits 10 ((digits 10 (Nat.succ a)) ++ (digits 10 (b))))
-| Int.ofNat a, Int.negSucc b => -Int.ofNat (fromDigits 10 ((digits 10 (a)) ++ (digits 10 (Nat.succ b))))
-
-def lastDigitWord (x : IntLike) : String :=
-  match (digits 10 (x.val.natAbs)).getLast? with
-  | some digit => match digit with
-     | 0 => "zero" -- zero
-     | 1 => "one" -- one
-     | 2 => "two" -- two
-     | 3 => "three" -- three
-     | 4 => "four" -- four
-     | 5 => "five" -- five
-     | 6 => "six" -- six
-     | 7 => "seven" -- seven
-     | 8 => "eight" -- eight
-     | 9 => "nine" -- nine
-     | _ => "zero"
-  | none => "zero"
-
-
-def isEven (x : ℤ) : Prop := x % 2 = 0
-
-theorem countDigits_add_of_lastDigitsOne (x y : IntLike)
-   (hx : lastDigitWord x = "one") (hy : lastDigitWord y = "one") : lastDigitWord (x + y) = "two" := by
-     simp_all [lastDigitWord]
-
-     have : (digits 10 x.val.natAbs).getLast? = some 1 := by
-       by_contra h
-       have : lastDigitWord x ≠ "one" := by
-        simp[lastDigitWord, h]
+def numeric_map (x : ℕ) : String := by
+  mod_cases h : x % 10
+  . exact ""
+  . exact "I"
+  . exact "oo"
+  . exact "ana"
+  . exact "swap"
+  . exact "straw"
+  . exact "redder"
+  . exact "rotator"
+  . exact "starrats"
+  . exact "birchcrab"
 
 
 
-#eval lastDigitWordCount (34 : IntLike)
---theorem concatEven (x y : ℤ) (hy : isEven x) : isEven (signedConcat x y) := by
-  --unfold signedConcat fromDigits
+theorem lsb_add_zero_one (x y : ℕ) (hx : lsb x = 0) (hy : lsb y = 1) : lsb (x + y) = 1 := by
+  revert hx hy
+  unfold lsb digit
+  simp
+  intro hx hy
+
 
 
 
